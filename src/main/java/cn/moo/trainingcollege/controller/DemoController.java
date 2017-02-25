9e0db12ca4ab0bb90c221ed17509a03ce43e06f2
xun <131250049@smail.nju.edu.cn>
2017-02-25 15:16:08 +0800

    filter test

---
+++ src/main/java/cn/moo/trainingcollege/controller/DemoController.java
@@ -0,0 +1,42 @@
+package cn.moo.trainingcollege.controller;
+
+import cn.moo.trainingcollege.entity.OrganizationEntity;
+import cn.moo.trainingcollege.entity.StudentEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by 小春 on 2017/2/25.
+ */
+
+@Controller
+public class DemoController {
+
+    @RequestMapping("/demo")
+    public String demo(){
+        return "demo";
+    }
+
+    @RequestMapping("/json")
+    @ResponseBody
+    public List<OrganizationEntity> getJson(){
+        OrganizationEntity entity = new OrganizationEntity();
+        entity.setName("name");
+        entity.setPassword("123456");
+        entity.setId("O0000002");
+        entity.setBalance(0);
+        List<OrganizationEntity> list = new ArrayList<OrganizationEntity>();
+        list.add(entity);
+        list.add(entity);
+        list.add(entity);
+        list.add(entity);
+        list.add(entity);
+        list.add(entity);
+        return list;
+
+    }
+}
--- src/main/webapp/demo.jsp
+++ src/main/webapp/demo.jsp
@@ -1,8 +1,8 @@
 <%--
   Created by IntelliJ IDEA.
-  User: chenm
-  Date: 2017/1/13
-  Time: 17:23
+  User: 小春
+  Date: 2017/2/25
+  Time: 11:20
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
@@ -11,6 +11,6 @@
     <title>Title</title>
 </head>
 <body>
-     <h1>Index</h1>
+     <h1>A Demo</h1>
 </body>
 </html>
